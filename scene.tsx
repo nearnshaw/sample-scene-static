14da558b67ae37795912bc01def26cfc3c5751db
matiasbargas <matiasbargas@gmail.com>
2018-07-08 18:19:10 -0300

    fixing model (#1)

--- scene.tsx
+++ scene.tsx
@@ -5,7 +5,7 @@ export default class HouseScene extends ScriptableScene {
   async render() {
     return (
       <scene>
-          <gltf-model position={{x:10, y:0, z:10}} rotation={{x:0, y:0, z:0}} scale={0.42} src="models/Forest/Forest_20x20.gltf" id="Forest"></gltf-model>
+          <gltf-model position={{x:10, y:0, z:10}} scale={0.42} src="models/Forest.gltf" id="Forest"></gltf-model>
       </scene>
     );
   }
