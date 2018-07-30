b2de7c39048e2a036d44ccbae05510618fce0745
Nicolas Earnshaw <earnshaw.nico@gmail.com>
2018-07-30 15:11:55 -0300

    Remove all ;
    In TypeScript it's optional to use ;
    We decided to remove them from all of our samples

--- scene.tsx
+++ scene.tsx
@@ -7,6 +7,6 @@ export default class HouseScene extends ScriptableScene {
       <scene>
           <gltf-model position={{x:10, y:0, z:10}} scale={0.42} src="models/Forest.gltf" id="Forest"></gltf-model>
       </scene>
-    );
+    )
   }
 }
